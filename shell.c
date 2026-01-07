9536f713d107cea283ce6397cfbbaebf4fafc278
leyvsi <sylviecosta78500@gmail.com>
2026-01-07 02:40:44 +0100

    update shell.c

--- shell.c
+++ shell.c
@@ -17,50 +17,75 @@ int main(void)
 
 	while (1)
 	{
-		/* display prompt if in interactive mode */
-		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
+/* display prompt if in interactive mode */
+	  if (isatty(STDIN_FILENO))
+              printf("#cisfun$ ");
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1) /* handle EOF (Ctrl+D) */
-		{
-			free(line);
-			exit(EXIT_SUCCESS);
+	      nread = getline(&line, &len, stdin);
+	  if (nread == -1) /* handle EOF (Ctrl+D) */
+	    {
+		free(line);
+		exit(EXIT_SUCCESS);
+	    }
+
+/* rm newline character */
+	  if (line[nread - 1] == '\n')
+	     line[nread - 1] = '\0';
+
+/* tokenize input into arguments */
+	  i = 0;
+            token = strtok(line, " ");
+            while (token != NULL)
+            {
+              argv = realloc(argv, sizeof(char *) * (i + 2));
+                if (!argv)
+                 {
+                   perror("realloc");
+                   exit(EXIT_FAILURE);
+                   }
+              argv[i] = strdup(token); /*duplucate token*/
+              i++;
+              token = strtok(NULL, " ");
+
+                }
+           if (argv)
+           argv[i] = NULL; /* null terminate array */
+
+
+/* execute command if input is not empty */
+           if (argv[0] != NULL)
+
+           if (handle_builtin(argv))
+             {
+               argv = NULL;
+               continue;
+             }
+
+
+             {
+	        child_pid = fork();
+		if (child_pid == 0) /* child process */
+	     {
+	        if (execve(argv[0], argv, environ) == -1)
+	     {
+		perror("hsh");
+		exit(EXIT_FAILURE);
 		}
+	     }
+	        else /* parent process */
+		     {
+		       waitpid(child_pid, &status, 0);
 
-		/* rm newline character */
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+                 free_args(argv);
+                 argv = NULL;
 
-		/* tokenize input into arguments */
-		i = 0;
-		token = strtok(line, " ");
-		while (token != NULL)
-		{
-			argv[i] = token;
-			token = strtok(NULL, " ");
-			i++;
-		}
-		argv[i] = NULL; /* null terminate the array */
+                      }
+		    }
 
-		/* execute command if input is not empty */
-		if (argv[0] != NULL)
-		{
-			child_pid = fork();
-			if (child_pid == 0) /* child process */
-			{
-				if (execve(argv[0], argv, environ) == -1)
-				{
-					perror("hsh");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else /* parent process */
-			{
-				waitpid(child_pid, &status, 0);
-			}
-		}
-	}
-	free(line);
-	return (0);
+             }
+	       free(line);
+               if (argv)
+                   free_args(argv);
+
+               return (0);
 }
