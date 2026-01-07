39fa656904d45eae992354332c6c0c5802211bf2
leyvsi <sylviecosta78500@gmail.com>
2026-01-07 03:07:26 +0100

    update shell.c

--- shell.c
+++ shell.c
@@ -17,85 +17,85 @@ int main(void)
 
 	while (1)
 	{
-/* display prompt if in interactive mode */
-	  if (isatty(STDIN_FILENO))
-              printf("#cisfun$ ");
+            /* display prompt if in interactive mode */
+	    if (isatty(STDIN_FILENO))
+                printf("#cisfun$ ");
 
-	      nread = getline(&line, &len, stdin);
-	  if (nread == -1) /* handle EOF (Ctrl+D) */
+	    nread = getline(&line, &len, stdin);
+	    if (nread == -1) /* handle EOF (Ctrl+D) */
 	    {
 		free(line);
                 if (argv)
-                    free_argv(argv);
+                    free_args(argv);
 		exit(EXIT_SUCCESS);
 	    }
 
-/* rm newline character */
+          /* rm newline character */
 	  if (line[nread - 1] == '\n')
 	     line[nread - 1] = '\0';
 
-/* tokenize input into arguments */
+          /* tokenize input into arguments */
 	  i = 0;
-            token = strtok(line, " ");
-            while (token != NULL)
-            {
+          token = strtok(line, " ");
+          while (token != NULL)
+          {
               argv = realloc(argv, sizeof(char *) * (i + 2));
-                if (!argv)
-                 {
-                   perror("realloc");
-                   exit(EXIT_FAILURE);
-                   }
+              if (!argv)
+              {
+                 perror("realloc");
+                 exit(EXIT_FAILURE);
+              }
               argv[i] = strdup(token); /*duplucate token*/
               i++;
               token = strtok(NULL, " ");
 
-                }
+           }
            if (argv)
-           argv[i] = NULL; /* null terminate array */
+              argv[i] = NULL; /* null terminate array */
 
 
-/* fofr skip empty input */
-           if (!argv || !argv[0]
+           /* for skip empty input */
+           if (!argv || !argv[0])
                continue;
 
- /* check for built-in commands */
+           /* check for built-in commands */
 
            if (handle_builtin(argv))
-             {
-               argv = NULL;
-               continue;
-             }
+           {
+              argv = NULL;
+              continue;
+           }
 
- /* execute external command */
+           /* execute external command */
 
-             {
-	        child_pid = fork();
-		if (child_pid == 0) /* child process */
-	     {
+           child_pid = fork();
+	   if (child_pid == 0) /* child process */
+	   {
 	        if (execve(argv[0], argv, environ) == -1)
-	     {
-		perror("hsh");
-		exit(EXIT_FAILURE);
+	        {
+		   perror("hsh");
+		   exit(EXIT_FAILURE);
 		}
-	     }
-	        else if (child_pid > 0)/* parent process */
-		     {
-		       waitpid(child_pid, &status, 0);
-                     }
-                     else
-                     {
-		       perror("fork");
+	    }
+	    else if (child_pid > 0)/* parent process */
+            {
+		waitpid(child_pid, &status, 0);
+            }
 
-                     }
+            else
+            {
+		perror("fork");
+
+            }
 
                  free_args(argv);
                  argv = NULL;
 
-                 }
+            }
 
-               free(line);
-               if (argv)
-                   free_args(argv);
+            free(line);
+            if (argv)
+               free_args(argv);
 
-               return (0);
+            return (0);
 }
